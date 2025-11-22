2e85b7c18c2c364ca6b947db04c165ac27880aa5
samith <samithfernando539@gmail.com>
2025-11-22 18:36:26 -0500

    add signin authentication

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Goals from './pages/Goals'
 import Navbar from './components/Navbar'
+import Authentication from './pages/Authentication'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/goals" element={<Goals />} />
         <Route path="/about" element={<About />} />
+        <Route path="/authentication" element={<Authentication />} />
       </Routes>
     </div>
   )
