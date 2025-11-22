9f59636851a2ed55996d6a113a5f9bd8479838d2
Dan <danielshan824@gmail.com>
2025-11-22 18:28:43 -0500

    All changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import Goals from './pages/Goals'
+import Goals from './pages/goals'
+import Authentication from './pages/Authentication'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/goals" element={<Goals />} />
         <Route path="/about" element={<About />} />
+        <Route path="/authentication" element={<Authentication />} />
       </Routes>
     </div>
   )
